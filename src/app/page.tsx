280cec1e971e63204145dd975ec3020730c12139
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-06-10 18:54:08 +0530

    changes
    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 import { Input } from "@/components/ui/input"
 import HomeHero from "@/components/homeHero/homeHero"
+import Testimonials from "@/components/testimonials/testimonials"
 
 export default function Home() {
   return (
 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <HomeHero/>
-     <h1>Resume Builder</h1>
-     <Input type="email" placeholder="Email" />
+      <Testimonials/>
     </main>
   );
 }
