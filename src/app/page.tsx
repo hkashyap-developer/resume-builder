00a8719094f55edcf637b6f213cfcd887f975473
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2024-06-10 16:14:59 +0530

    Changes
    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 
+import { Input } from "@/components/ui/input"
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1>Resume Builder</h1>
+     <Input type="email" placeholder="Email" />
     </main>
   );
 }
